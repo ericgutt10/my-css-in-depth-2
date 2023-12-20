61d0f6403eac2231a4a413f62971198eb869e963
eric-gutt <eric.guttormson@live.com>
2023-12-19 22:35:03 -0600

    Initial Commit

---
+++ Core/assets/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../Components/**/*',
+    '../Features/**/*' 
+  ],
+  theme: {
+    extend: {
+       
+    },
+  },
+  plugins: [],
+}
+
